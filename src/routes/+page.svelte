4f80b70be858723daae7b35ec8bf919d4695936f
gussmarconi <guss.marconi@outlook.com>
2025-02-06 16:02:04 -0400

    Proyecto Sveltekit + Skeleton configurado

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,6 @@
 <h1>Welcome to your library project</h1>
 <p>Create your package using @sveltejs/package and preview/showcase your work with SvelteKit</p>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<div class="card preset-filled-surface-100-900 border-[1px] border-surface-200-800 w-full max-w-md p-4 text-center">
+    <p>Card</p>
+  </div>
